07bf7a49d45443859b8376ddf88bab34fb57c320
yaseenssenyonjo <yaseenssenyonjo@gmail.com>
2019-02-17 17:33:20 +0000

    Filled the missing documentation of parameters.

--- ForwardProxy/Networking/Responses/ConnectionResponse.cs
+++ ForwardProxy/Networking/Responses/ConnectionResponse.cs
@@ -17,7 +17,7 @@ namespace ForwardProxy.Networking.Responses
         /// <summary>
         /// Initialises a new instance of the <see cref="ConnectionResponse"/> class.
         /// </summary>
-        /// <param name="success"></param>
+        /// <param name="success">A value indicating whether the client connected successfully.</param>
         public ConnectionResponse(bool success)
         {
             Success = success;
@@ -26,8 +26,8 @@ namespace ForwardProxy.Networking.Responses
         /// <summary>
         /// Initialises a new instance of the <see cref="ConnectionResponse"/> class.
         /// </summary>
-        /// <param name="success"></param>
-        /// <param name="message"></param>
+        /// <param name="success">A value indicating whether the client connected successfully.</param>
+        /// <param name="message">The exception message.</param>
         public ConnectionResponse(bool success, string message) : this(success)
         {
             Message = message;
